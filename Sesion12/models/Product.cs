b6540f096f3cadedba64d2f09fd4162626f7c1e5
abojii <abojorgec@gmail.com>
2024-09-27 10:05:25 -0600

    Agregar estructura producto

---
+++ Sesion12/models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion12.models
+{
+    public struct Product
+    {
+         public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } 
+        public string ProductId { get; set; }
+
+
+    }
+}
